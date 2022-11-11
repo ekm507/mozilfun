b9c869dd84f5e1b7a8c985bd365bdf23dc214f9c
Erfan Kheyrollahi <ekm507@gmail.com>
2022-11-11 13:11:57 +0330

    add a new route for query

--- mozilfun.py
+++ mozilfun.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from requests import get
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ query_html_template = open('html/query.html').read()
 @app.route('/')
 def get_home():
     return homepage_html
+
+@app.route('/s/<query>')  # type: ignore
+def query_applets(query:str):
+    search_page = get(f'https://addons.mozilla.org/en-US/firefox/search/?q={query}').text
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
